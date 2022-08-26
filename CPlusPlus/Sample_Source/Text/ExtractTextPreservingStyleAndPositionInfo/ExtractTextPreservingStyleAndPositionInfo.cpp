9cc731b304843a100e58e198e693bedc5c2cfafc
bruceh <bruceh@datalogics.com>
2022-08-26 16:45:08 -0500

    Add new ExtractText samples

---
+++ CPlusPlus/Sample_Source/Text/ExtractTextPreservingStyleAndPositionInfo/ExtractTextPreservingStyleAndPositionInfo.cpp
@@ -0,0 +1,126 @@
+//
+// Copyright (c) 2022, Datalogics, Inc. All rights reserved.
+//
+// For complete copyright information, refer to:
+// http://dev.datalogics.com/adobe-pdf-library/license-for-downloaded-pdf-samples/
+//
+// This sample extracts text, quads, and styles from a PDF
+// document and saves the text to a file.
+//
+// For more detail see the description of the ExtractTextPreservingStyleAndPositionInfo
+// sample program on our Developer’s site,
+// http://dev.datalogics.com/adobe-pdf-library/sample-program-descriptions/c1samples
+
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "InitializeLibrary.h"
+#include "APDFLDoc.h"
+#include "TextExtract.h"
+
+#ifdef MAC_PLATFORM
+#include "../../_Common/ThirdParty/json.hpp"
+#else
+#include "ThirdParty/json.hpp"
+#endif
+
+// Set the namespace for the JSON library
+using json = nlohmann::json;
+
+const char *DEF_INPUT = "../../../../Resources/Sample_Input/sample.pdf";
+const char *DEF_OUTPUT = "ExtractTextPreservingStyleAndPositionInfo-out.json";
+
+void SaveJson(std::vector<PDTextAndDetailsExtractRec> extractedText);
+
+int main(int argc, char **argv) {
+
+    // Initialize the library
+    APDFLib libInit;
+    ASErrorCode errCode = 0;
+    if (libInit.isValid() == false) {
+        errCode = libInit.getInitError();
+        std::cout << "Initialization failed with code " << errCode << std::endl;
+        return libInit.getInitError();
+    }
+
+    DURING
+
+        APDFLDoc inAPDoc(DEF_INPUT, true);
+
+        TextExtract textExtract(inAPDoc.getPDDoc());
+
+        std::vector<PDTextAndDetailsExtractRec> extractedText = textExtract.GetTextAndDetails();
+
+        SaveJson(extractedText);
+
+    HANDLER
+        errCode = ERRORCODE;
+        libInit.displayError(errCode);
+    END_HANDLER
+
+    return errCode;
+}
+
+//  Helper function to set decimal precision in JSON file
+std::string to_string_with_precision(float a_value, const int n = 2)
+{
+    std::ostringstream out;
+    out.precision(n);
+    out << std::fixed << a_value;
+    return out.str();
+}
+
+// Create JSON file of results
+void SaveJson(std::vector<PDTextAndDetailsExtractRec> extractedText)
+{
+    // This array will hold the JSON stream that we will print to the output JSON file.
+    json result = json::array();
+
+    for (size_t textIndex = 0; textIndex < extractedText.size(); ++textIndex) {
+
+        json textObject = json::object();
+        textObject["text"] = extractedText[textIndex].text;
+        for (size_t quadIndex = 0; quadIndex < extractedText[textIndex].boundingQuads.size(); ++quadIndex) {
+            json quadPointValues = json::array();
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].tl.h));
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].tl.v));
+            textObject["quad"]["top-left"] = quadPointValues;
+            quadPointValues.clear();
+
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].tr.h));
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].tr.v));
+            textObject["quad"]["top-right"] = quadPointValues;
+            quadPointValues.clear();
+
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].bl.h));
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].bl.v));
+            textObject["quad"]["bottom-left"] = quadPointValues;
+            quadPointValues.clear();
+
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].br.h));
+            quadPointValues.push_back(to_string_with_precision(extractedText[textIndex].boundingQuads[quadIndex].br.v));
+            textObject["quad"]["bottom-right"] = quadPointValues;
+            quadPointValues.clear();
+        }
+        json styles = json::array();
+        for (size_t styleIndex = 0; styleIndex < extractedText[textIndex].styles.size(); ++styleIndex) {
+            json styleObject = json::object();
+            styleObject["char-index"] = extractedText[textIndex].styles[styleIndex].charIndex;
+            styleObject["font-name"] = extractedText[textIndex].styles[styleIndex].fontname;
+            styleObject["font-size"] = to_string_with_precision(extractedText[textIndex].styles[styleIndex].fontsize);
+            styleObject["color-space"] = extractedText[textIndex].styles[styleIndex].colorValues.DLSpace;
+            json colorValues = json::array();
+            for (size_t colorIndex = 0; colorIndex < 4; ++colorIndex) {
+                colorValues.push_back(extractedText[textIndex].styles[styleIndex].colorValues.DLColor[colorIndex]);
+            }
+            styleObject["color-values"] = colorValues;
+            styles.push_back(styleObject);
+        }
+        textObject["styles"] = styles;
+        result.push_back(textObject);
+    }
+    std::ofstream jsonOutputFileStream(DEF_OUTPUT, std::ios_base::trunc | std::ios_base::out);
+    jsonOutputFileStream << std::setw(4) << result;
+    jsonOutputFileStream.close();
+}
---
+++ DotNETCore/Sample_Source/Text/ExtractTextFromMultiRegions/ExtractTextFromMultiRegions.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Datalogics.PDFL;
+using ExtractTextNameSpace;
+
+/*
+ * This sample processes PDF files in a folder and extracts text from specific regions
+ * of its pages and saves the text to a CSV file.
+ * 
+ * Copyright (c) 2022, Datalogics, Inc. All rights reserved.
+ *
+ * For complete copyright information, refer to:
+ * http://dev.datalogics.com/adobe-pdf-library/license-for-downloaded-pdf-samples/
+ *
+ */
+namespace ExtractTextFromMultiRegions
+{
+    class ExtractTextFromMultiRegions
+    {
+        // Set Defaults
+        static String sInput = Library.ResourceDirectory + "Sample_Input/ExtractTextFromMultiRegions";
+        static String sOutput = "../ExtractTextFromMultiRegions-out.csv";
+
+        // Rectangular regions to extract text in points (origin of the page is bottom left)
+        // (Left, Right, Bottom, Top)
+        static double[] invoice_number = { 500, 590, 692, 710 };
+        static double[] date = { 500, 590, 680, 700 };
+        static double[] order_number = { 500, 590, 672, 688 };
+        static double[] cust_id = { 500, 590, 636, 654 };
+        static double[] total = { 500, 590, 52, 73 };
+
+        static List<double[]> regions = new List<double[]>
+        {
+            invoice_number,
+            date,
+            order_number,
+            cust_id,
+            total
+        };
+        enum quadSide { left, right, bottom, top };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ExtractTextFromMultiRegions Sample:");
+
+            using (Library lib = new Library())
+            {
+                Console.WriteLine("Initialized the library.");
+                using (System.IO.StreamWriter outfile = new System.IO.StreamWriter(sOutput))
+                {
+                    Console.WriteLine("Writing to: " + sOutput);
+                    // Print out a heading for CSV file
+                    outfile.WriteLine("Filename,Invoice Number,Date,Order Number,Customer ID,Total");
+
+                    string[] pdfFiles = Directory.GetFiles(sInput);
+
+                    foreach (string fileName in pdfFiles)
+                    {
+                        using (Document doc = new Document(fileName))
+                        {
+                            outfile.Write(System.IO.Path.GetFileName(fileName));
+                            Console.WriteLine("Input file: " + System.IO.Path.GetFileName(fileName));
+
+                            using (ExtractText docText = new ExtractText(doc))
+                            {
+                                for (int pageNum = 0; pageNum < doc.NumPages; ++pageNum)
+                                {
+                                    List<TextAndDetailsObject> result = docText.GetTextAndDetails(pageNum);
+                                    foreach (double[] region in regions)
+                                    {
+                                        outfile.Write(",");
+                                        foreach (TextAndDetailsObject textInfo in result)
+                                        {
+                                            bool allQuadsWithinRegion = true;
+                                            // A Word typically has only 1 quad, but can have more than one
+                                            // for hyphenated words, words on a curve, etc.
+                                            foreach (Quad quad in textInfo.Quads)
+                                            {
+                                                if (!CheckWithinRegion(quad, region))
+                                                {
+                                                    allQuadsWithinRegion = false;
+                                                    break;
+                                                }
+                                            }
+                                            if (allQuadsWithinRegion)
+                                            {
+                                                // Put this Word that meets our criteria in the output document
+                                                outfile.Write(textInfo.Text);
+                                            }
+                                        }
+                                    }
+                                    outfile.WriteLine();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // For this sample, we will consider a Word to be in the region of interest if the
+        // complete Word fits within the specified rectangular box
+        static bool CheckWithinRegion(Quad wordQuad, double[] region)
+        {
+            if (wordQuad.BottomLeft.H >= region[(int)quadSide.left] && wordQuad.BottomRight.H <= region[(int)quadSide.right] &&
+                wordQuad.TopLeft.H >= region[(int)quadSide.left] && wordQuad.TopRight.H <= region[(int)quadSide.right] &&
+                wordQuad.BottomLeft.V >= region[(int)quadSide.bottom] && wordQuad.TopLeft.V <= region[(int)quadSide.top] &&
+                wordQuad.BottomRight.V >= region[(int)quadSide.bottom] && wordQuad.TopRight.V <= region[(int)quadSide.top])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
